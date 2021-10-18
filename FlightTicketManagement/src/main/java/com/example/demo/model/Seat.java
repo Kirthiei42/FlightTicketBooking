2e819a1da14eda6595004d04a378cf5e5a845add
Kirthiei42 <kirthiei42@gmail.com>
2021-10-18 15:22:37 +0530

    Update Seat.java
    2nd

--- FlightTicketManagement/src/main/java/com/example/demo/model/Seat.java
+++ FlightTicketManagement/src/main/java/com/example/demo/model/Seat.java
@@ -1,8 +1,10 @@
 package com.example.demo.model;
-
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Enitity
 public class Seat {
 	@Id
-	private int SeatId
+	private int SeatId;
 	private int SeatType;
 
 }
